8ed6babc3711d10904194a90caa391ba6b3e9e98
LAFisherGIS <92408705+LAFisherGIS@users.noreply.github.com>
2022-02-15 21:29:49 -0800

    I'm going to scream holy fuck what is wrong with you

--- javascript.js
+++ javascript.js
@@ -257,7 +257,7 @@ function createFormPopup() {
   				'</fieldset>' +
   			'</li>' +
         '<li>' +
-          '<input type="submit" id="submit">'
+          '<input type="Submit" id="submit">' +
         '</li>' +
   	 '</ul>' +
   	'</form>'
